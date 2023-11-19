d89f55af0fabba713763adb5cc1776aceff438d7
Mateus Nosse <80108925+MNosse@users.noreply.github.com>
2023-11-19 16:42:36 -0300

    adding logger and swagger

--- src/main/kotlin/br/com/udesc/ceavi/eso/ese/movieservice/MovieServiceApplication.kt
+++ src/main/kotlin/br/com/udesc/ceavi/eso/ese/movieservice/MovieServiceApplication.kt
@@ -1,11 +1,28 @@
 package br.com.udesc.ceavi.eso.ese.movieservice
 
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Info
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 
 @SpringBootApplication
 class MovieServiceApplication
 
 fun main(args: Array<String>) {
-	runApplication<MovieServiceApplication>(*args)
+    runApplication<MovieServiceApplication>(*args)
+}
+
+@Configuration
+class OpenApiConfig() {
+    @Bean
+    fun usersMicroserviceOpenApi(): OpenAPI {
+        return OpenAPI()
+            .info(
+                Info().title("Movie-Web-Service")
+                    .description("A Movie web service")
+                    .version("1.0")
+            )
+    }
 }
